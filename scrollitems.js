967184b2c40c2ff8de58d3f5b1b73f97c6b9a730
MyLangdy11 <128284019+MyLangdy11@users.noreply.github.com>
2024-05-04 23:10:08 +0700

    Add files via upload

--- scrollitems.js
+++ scrollitems.js
@@ -44,23 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const walk = (x - startX) * 2; // Adjust the scroll speed
     scrollingContainer.scrollLeft = scrollLeft - walk;
   });
-
-  // Bottom menu
-
-  // Add an active class to the clicked menu item
-  const menuItems = document.querySelectorAll('.menu-item');
-
-  menuItems.forEach(item => {
-    item.addEventListener('click', (e) => {
-      e.preventDefault();
-      menuItems.forEach(item => {
-        item.classList.remove('active');
-        item.querySelector('i').classList.remove('active');
-      });
-      item.classList.add('active');
-      item.querySelector('i').classList.add('active');
-    });
-  });
 });
 
 // hide or show
@@ -73,7 +56,7 @@ window.addEventListener('scroll', () => {
   if (prevScrollPos > currentScrollPos) {
     menu.style.bottom = '0';
   } else {
-    menu.style.bottom = '-60px';
+    menu.style.bottom = '-65px';
   }
 
   prevScrollPos = currentScrollPos;
